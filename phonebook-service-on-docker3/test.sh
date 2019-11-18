d858bd097846d31a49afddb83458ca0df72f674b
Rifqi <rifqiaramadhan@gmail.com>
2019-11-18 17:00:02 +0700

    bug

--- phonebook-service-on-docker3/test.sh
+++ phonebook-service-on-docker3/test.sh
@@ -2,12 +2,12 @@
 #this is bash
 token=$(curl -s 'http://127.0.0.1:9990/auth' -X POST -d '{"username": "leomessi", "password": "kaoskakimerah123"}' | \
     python3 -c "import sys, json; print(json.load(sys.stdin)['token'])")
-curl -v http://127.0.0.1:9990/phonebook/c339df66-04d1-11ea-a7f5-c9aa0ab4e96b -H "Authorization:${token}" | python3 -c "import sys, json; print('response status: ' + json.load(sys.stdin)['status']); print('response data: ' + json.load(sys.stdin)['data'])"
+curl -s 'http://127.0.0.1:9990/phonebook/c339df66-04d1-11ea-a7f5-c9aa0ab4e96b' -H "Authorization:${token}" | python3 -c "import sys, json; print('response status: ' + json.load(sys.stdin)['status']); print('response data: ' + str(json.load(sys.stdin)['data']))"
 
 token2=$(curl -s 'http://127.0.0.1:9991/auth' -X POST -d '{"username": "leomessi", "password": "kaoskakimerah123"}' | \
     python3 -c "import sys, json; print(json.load(sys.stdin)['token'])")
-curl -v http://127.0.0.1:9991/phonebook/c339df66-04d1-11ea-a7f5-c9aa0ab4e96b -H "Authorization:${token2}" | python3 -c "import sys, json; print('response status: ' + json.load(sys.stdin)['status']); print('response data: ' + json.load(sys.stdin)['data'])"
+curl -s 'http://127.0.0.1:9991/phonebook/c339df66-04d1-11ea-a7f5-c9aa0ab4e96b' -H "Authorization:${token2}" | python3 -c "import sys, json; print('response status: ' + json.load(sys.stdin)['status']); print('response data: ' + str(json.load(sys.stdin)['data']))"
 
 token3=$(curl -s 'http://127.0.0.1:9992/auth' -X POST -d '{"username": "leomessi", "password": "kaoskakimerah123"}' | \
     python3 -c "import sys, json; print(json.load(sys.stdin)['token'])")
-curl -v http://127.0.0.1:9992/phonebook/c339df66-04d1-11ea-a7f5-c9aa0ab4e96b -H "Authorization:${token3}" | python3 -c "import sys, json; print('response status: ' + json.load(sys.stdin)['status']); print('response data: ' + json.load(sys.stdin)['data'])"
+curl -s 'http://127.0.0.1:9992/phonebook/c339df66-04d1-11ea-a7f5-c9aa0ab4e96b' -H "Authorization:${token3}" | python3 -c "import sys, json; print('response status: ' + json.load(sys.stdin)['status']); print('response data: ' + str(json.load(sys.stdin)['data']))"
