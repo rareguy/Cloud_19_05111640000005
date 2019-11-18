840de8989252051941c2d2f3bf957731bff33e82
Rifqi <rifqiaramadhan@gmail.com>
2019-11-18 17:13:14 +0700

    ga jadi

--- phonebook-service-on-docker3/test.sh
+++ phonebook-service-on-docker3/test.sh
@@ -3,15 +3,14 @@
 token=$(curl -s 'http://127.0.0.1:9990/auth' -X POST -d '{"username": "leomessi", "password": "kaoskakimerah123"}' | \
     python3 -c "import sys, json; print(json.load(sys.stdin)['token'])")
 echo ${token}
-curl -s 'http://127.0.0.1:9990/phonebook/c339df66-04d1-11ea-a7f5-c9aa0ab4e96b' -H "Authorization:${token}" | python3 -c "import sys, json; print('response status: ' + json.load(sys.stdin)['status']); print('response data: ' + json.load(json.load(sys.stdin)['data']))"
-
+curl -s 'http://127.0.0.1:9990/phonebook/c339df66-04d1-11ea-a7f5-c9aa0ab4e96b' -H "Authorization:${token}"
 
 token2=$(curl -s 'http://127.0.0.1:9991/auth' -X POST -d '{"username": "leomessi", "password": "kaoskakimerah123"}' | \
     python3 -c "import sys, json; print(json.load(sys.stdin)['token'])")
 echo ${token2}
-curl -s 'http://127.0.0.1:9991/phonebook/c339df66-04d1-11ea-a7f5-c9aa0ab4e96b' -H "Authorization:${token2}" | python3 -c "import sys, json; print('response status: ' + json.load(sys.stdin)['status']); print('response data: ' + json.load(json.load(sys.stdin)['data']))"
+curl -s 'http://127.0.0.1:9991/phonebook/c339df66-04d1-11ea-a7f5-c9aa0ab4e96b' -H "Authorization:${token2}"
 
 token3=$(curl -s 'http://127.0.0.1:9992/auth' -X POST -d '{"username": "leomessi", "password": "kaoskakimerah123"}' | \
     python3 -c "import sys, json; print(json.load(sys.stdin)['token'])")
 echo ${token3}
-curl -s 'http://127.0.0.1:9992/phonebook/c339df66-04d1-11ea-a7f5-c9aa0ab4e96b' -H "Authorization:${token3}" | python3 -c "import sys, json; print('response status: ' + json.load(sys.stdin)['status']); print('response data: ' + json.load(json.load(sys.stdin)['data']))"
+curl -s 'http://127.0.0.1:9992/phonebook/c339df66-04d1-11ea-a7f5-c9aa0ab4e96b' -H "Authorization:${token3}"
