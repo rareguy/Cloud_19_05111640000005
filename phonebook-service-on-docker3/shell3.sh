da7256d95df45ca574310a5be78928e8013216f0
Rifqi <rifqiaramadhan@gmail.com>
2019-11-18 16:44:30 +0700

    bug

--- phonebook-service-on-docker3/shell3.sh
+++ phonebook-service-on-docker3/shell3.sh
@@ -1,5 +1,5 @@
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-sudo docker rm -f pservice
+sudo docker rm -f pservice3
 sudo docker run --name pservice3 -p 9992:5000 --env REDISADDR=127.0.0.1 phonebook-docker-2:1.0
